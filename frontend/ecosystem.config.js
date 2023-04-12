10cad2b18779e0549509340aff8d8731abbbddd6
daryamakavchik <darjamakavchik@gmail.com>
2023-04-12 12:02:35 -0700

    fffff

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'git@github.com:daryamakavchik/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'post-deploy': 'rm -rf node_modules && npm cache clean --force && npm install && npm init && npm run build'
+      'post-deploy': 'npm install && npm run build'
     },
   },
 };
