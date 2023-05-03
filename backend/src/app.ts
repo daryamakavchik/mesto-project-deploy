8f07593a7955f4b95db52856ef2089cd299b8248
daryamakavchik <darjamakavchik@gmail.com>
2023-05-03 06:15:44 -0700

    <3<3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import Joi = require('joi');
 import { celebrate } from 'celebrate';
 import mongoose from 'mongoose';
@@ -62,12 +63,7 @@ app.use(limiter);
 app.use(helmet());
 app.use(requestLogger);
 
-app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.send(200);
-});
+app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
