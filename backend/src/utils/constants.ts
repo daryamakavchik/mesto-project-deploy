92f9a12953bb2ca19e9532e220dbf6085f2b1dfe
daryamakavchik <darjamakavchik@gmail.com>
2023-05-03 05:12:12 -0700

    I wish you loved me as much

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -5,5 +5,5 @@ export const STATUS_403 = 403;
 export const STATUS_404 = 404;
 export const STATUS_409 = 409;
 export const STATUS_11000 = 11000;
-export const { PORT = 8080, LINK = 'mongodb://localhost:27017/mestodb', SECRET_KEY = 'some-secret-key' } = process.env;
+export const { PORT = 8080, LINK = 'mongodb://127.0.0.1:27017/mestodb', SECRET_KEY = 'some-secret-key' } = process.env;
 export const linkRegex = /^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/;
