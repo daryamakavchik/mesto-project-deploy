e87390884a37292f2f85f8d7b2312bcb56450c86
daryamakavchik <darjamakavchik@gmail.com>
2023-05-05 14:18:49 -0700

    fix: signin link

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -64,6 +64,7 @@ class Api {
 const api = new Api({
   baseUrl: "https://nomoreparties.co/v1/plus-cohort-8",
   credentials: 'include',
+
   headers: {
       authorization: "837c0be1-5609-4c04-b384-491cd26df7eb",
       "Content-Type": "application/json",
