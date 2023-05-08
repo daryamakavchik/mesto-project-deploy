aa8de12a0eadb5a960cfd778b8408352ecb25fc8
daryamakavchik <darjamakavchik@gmail.com>
2023-05-08 13:58:28 -0700

    fix main

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -20,13 +20,13 @@ function Main({
           <div className="profile__overlay" onClick={onEditAvatar}></div>
           <img
             className="profile__image"
-            src={currentUser.avatar}
+            src={currentUser.data.avatar}
             alt="Аватар пользователя"
           />
         </div>
         <div className="profile__info">
-          <h1 className="profile__name">{currentUser.name}</h1>
-          <p className="profile__about">{currentUser.about}</p>
+          <h1 className="profile__name">{currentUser.data.name}</h1>
+          <p className="profile__about">{currentUser.data.about}</p>
           <button
             className="button profile__button-edit"
             type="button"
