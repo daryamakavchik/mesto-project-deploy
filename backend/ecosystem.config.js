72718f93cc641503d488f31da68eda4040a72a9a
daryamakavchik <darjamakavchik@gmail.com>
2023-05-08 18:17:40 -0700

    specify json config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && npm install && npm run build && pm2 startOrRestart',
+      'post-deploy': 'cd backend && npm install && npm run build && pm2 startOrRestart ecosystem.config.json',
     },
   },
 };
